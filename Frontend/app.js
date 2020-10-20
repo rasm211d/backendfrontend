6f883d2d17f64f1048c845a46b11257bd625f359
Rasmus Jannerup <rasm211d@edu.easj.dk>
2020-10-20 11:51:45 +0200

    everything is working :))))

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, 'wwwroot')));
 
@@ -8,6 +10,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.listen(3000, () => {
-    console.log("ESKETIT!");
-})
+app.listen(PORT, HOST);
